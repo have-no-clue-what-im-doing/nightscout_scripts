279b3d7e7bb5da8168682301da08aa619e7407d4
Broderic Duncan <brodericjduncan@gmail.com>
2025-08-25 19:14:55 -0400

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -6,6 +6,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confnew" upgrade
 sudo curl -fsSl https://get.docker.com | bash
-sudo usermod -aG docker "$USER"
+sudo usermod -aG docker "$SUDO_USER"
 wget https://raw.githubusercontent.com/have-no-clue-what-im-doing/nightscout_scripts/refs/heads/main/docker-compose.yaml
 sudo reboot
