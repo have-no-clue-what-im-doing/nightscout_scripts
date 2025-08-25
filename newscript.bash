896aa293f4027164eff0414fbd8b5e167c7fbfc7
Broderic Duncan <brodericjduncan@gmail.com>
2025-08-25 14:43:01 -0400

    Update newscript.bash

--- newscript.bash
+++ newscript.bash
@@ -5,3 +5,4 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confnew" upgrade
 sudo curl -fsSl https://get.docker.com | bash
+wget https://raw.githubusercontent.com/have-no-clue-what-im-doing/nightscout_scripts/refs/heads/main/docker-compose.yaml
